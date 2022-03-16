17bdb23d098a1b77ba59456c04b826a14f6249b1
obaidurx <mdobaidurbd@gmail.com>
2022-03-17 00:37:00 +0600

    create feedback ui & others styles

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
-  const title = "Blog Post";
-  const body = "This is my blog Post";
-  const comments = [
-    { id: 1, text: "comment one" },
-    { id: 2, text: "comment two" },
-    { id: 3, text: "comment three" },
-  ];
-  const loading = false;
-  const showComments = true;
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments:{comments.length}</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-  if (loading) return <h1>Loading .....</h1>;
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
   );
 }
+
 export default App;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor saepe eveniet, labore non quam odit, suscipit esse accusantium repudiandae laudantium unde. Totam in reprehenderit sapiente dolorum repudiandae at earum!",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor saepe eveniet, labore non quam odit!",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor saepe eveniet, labore non quam odit, suscipit esse accusantium ",
+  },
+];
+
+export default FeedbackData;
