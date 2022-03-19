64f027969e4c81910bf48110b760245e58a48469
obaidurx <mdobaidurbd@gmail.com>
2022-03-20 00:19:36 +0600

    added feedback rating & feedback input field

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 import { useState } from "react";
 import React from "react";
 import FeedbackList from "./components/FeedbackList";
@@ -9,6 +11,11 @@ import FeedbackForm from "./components/FeedbackForm";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure want to delete ?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -18,7 +25,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,14 +1,14 @@
 import React,{useState} from 'react';
 
 const RatingSelect = ({select}) => {
-    const [selected,setSelected]=useState(2)
+    const [selected,setSelected]=useState(10)
     const handleChange=(e)=>{
         setSelected(+e.target.value);
         select(+e.target.value)
     }
     return (
         <div className='rating'>
-            <ul>
+            <ul className='rating'>
                 <li>
                 <input 
                 type="radio"
@@ -30,6 +30,74 @@ const RatingSelect = ({select}) => {
                 checked={selected===2}
                 />
                 <label htmlFor="num2">2</label>
+                </li>
+                <li>
+                <input 
+                type="radio"
+                id='num3'
+                name='rating'
+                value='3'
+                onChange={handleChange}
+                checked={selected===3}
+                />
+                <label htmlFor="num3">3</label>
+                </li>
+                <li>
+                <input 
+                type="radio"
+                id='num3'
+                name='rating'
+                value='3'
+                onChange={handleChange}
+                checked={selected===4}
+                />
+                <label htmlFor="num4">4</label>
+                </li>
+                <li>
+                <input 
+                type="radio"
+                id='num7'
+                name='rating'
+                value='7'
+                onChange={handleChange}
+                checked={selected===7}
+                />
+                <label htmlFor="num7">7</label>
+
+                </li>
+                <li>
+                <input 
+                type="radio"
+                id='num8'
+                name='rating'
+                value='8'
+                onChange={handleChange}
+                checked={selected==8}
+                />
+                <label htmlFor="num8">8</label>
+                </li>
+                <li>
+                <input 
+                type="radio"
+                id='num9'
+                name='rating'
+                value='9'
+                onChange={handleChange}
+                checked={selected===9}
+                />
+                <label htmlFor="num9">9</label>
+
+                </li>
+                <li>
+                <input 
+                type="radio"
+                id='num10'
+                name='rating'
+                value='10'
+                onChange={handleChange}
+                checked={selected===10}
+                />
+                <label htmlFor="num10">10</label>
                 </li>
             </ul>
         </div>
